76f7f3ccd3f6545c9ebe2d96d0c9f411aa60cada
triptaminer <triptaminer@gmail.com>
2022-11-10 12:16:22 +0100

    added JPA playground package

---
+++ src/main/java/jpa/playground/SpringClientPlay.java
@@ -0,0 +1,25 @@
+package jpa.playground;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+//@Configuration
+public class SpringClientPlay {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringClientPlay.class);
+    }
+
+    @Bean
+    public CommandLineRunner runnerSimple(){
+        return args -> {
+            System.out.println("jpaaaaa");
+        };
+
+    }
+
+
+
+}
